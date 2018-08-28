1efa3229af07066780d5666f2e40fc8a43edab89
Yoji Minabe <minabe@hojicha.jp>
2018-08-28 17:20:14 +0900

    第22回_シークレットトークンは俺が守る！PepperにCloud Functions経由でツイートさせてみた！

---
+++ 第22回_シークレットトークンは俺が守る！PepperにCloud Functions経由でツイートさせてみた！/html/js/contents.js
@@ -0,0 +1,33 @@
+﻿// pepper contents
+
+Tapper.contents = {
+    isRunning: false,
+    onLoad: function (param) {
+        console.log("onLoad.");
+    },
+
+    onStart: function (param) {
+        console.log("onStart.");
+        Tapper.preview.init({ id: "preview" });
+    },
+
+    onPageSplash: function (param) {
+        console.log("onPageSplash.");
+
+    },
+    onPageWaiting: function (param) {
+        console.log("onPageWating.");
+
+    },
+    onPagePreview: function (param) {
+        console.log("onPagePreview.");
+        if (!Tapper.contents.isRunning) {
+            Tapper.contents.isRunning = true;
+            Tapper.preview.start();
+        }
+    },
+    onPageHashTag: function (param) {
+        console.log("onPageHashTag.");
+
+    }
+}
