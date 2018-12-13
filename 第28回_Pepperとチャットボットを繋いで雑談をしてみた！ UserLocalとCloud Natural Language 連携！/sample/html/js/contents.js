d29b4acf21ddb26b66d9db80e8cab48bbc371144
Yoji Minabe <minabe@hojicha.jp>
2018-12-13 13:23:45 +0900

    第26回、第28回追加

---
+++ 第28回_Pepperとチャットボットを繋いで雑談をしてみた！ UserLocalとCloud Natural Language 連携！/sample/html/js/contents.js
@@ -0,0 +1,33 @@
+﻿// pepper contents
+
+Tapper.contents = {
+    isRunning: false,
+    onLoad: function (param) {
+        console.log("onLoad.");
+    },
+
+    onStart: function (param) {
+        console.log("onStart.");
+    },
+
+    onPageSplash: function (param) {
+        console.log("onPageSplash.");
+
+    },
+    onPageWaiting: function (param) {
+        console.log("onPageWating.");
+        Tapper.utl.subscribeEvent("ComSoftbankrobotics/SampleProject/NaturalLanguage/text", function (text) {
+            $('#text').prepend(text);
+            $('#text').prepend("<br />");
+        });
+        Tapper.utl.subscribeEvent("ComSoftbankrobotics/SampleProject/NaturalLanguage/score", function (text) {
+            $('#text').prepend('<span style="color: #f00">' + text + '</span>');
+            $('#text').prepend("<br />");
+        });
+
+    },
+    onPageHashTag: function (param) {
+        console.log("onPageHashTag.");
+
+    }
+}
